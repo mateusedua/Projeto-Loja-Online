e073493891df3fad90197eefff4961a65976e2eb
Mateus Edurdo <mateuseduardoteuta10@gmail.com>
2022-08-07 23:44:51 -0300

    Procedure pra realizar o insert na tabela cliente

--- procedures/Cadastracliente.sql
+++ procedures/Cadastracliente.sql
@@ -6,3 +6,31 @@ as
 
 
 
+declare @cli_id_cliente numeric(18,0) = null
+
+
+
+begin try
+	begin transaction 
+		
+		select @cli_id_cliente = next value for cliente_seq
+
+		insert into cliente(cli_email,
+							cli_senha)
+		values(	@email,
+				@senha)
+
+	commit transaction
+end try
+
+begin catch
+	rollback transaction 
+
+	insert into log_procedimentos(	error_line,
+									error_procedure,
+									error_message)
+	values(	error_line(),
+			ERROR_PROCEDURE(),
+			ERROR_MESSAGE())
+
+end catch
